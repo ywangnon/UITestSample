ac5c41cec8e3c720dc10ea0f19a0bfce8183eddb
ywangnon <ywangnon123@gmail.com>
2022-10-31 23:26:17 +0900

    Initial Commit

---
+++ UITestSample/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  UITestSample
+//
+//  Created by Hansub Yoo on 2022/10/31.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
